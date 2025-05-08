ffa3bebfeaef55f432403c33b242ffb3224167b5
LucasVaninii <lucassvaninii@gmail.com>
2025-05-07 21:41:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-function pesquisaSatisfacao(){
-   
-    let nota // guarda as notas
-    let conta = 1 // conta de 1 a 10
-    let soma = 0
-    let satisfacao = 0
-    let insatisfacao = 0
+function pesquisaSatisfacao() {
 
-    while(conta <= 10){
-        // Number - converte o dado informado de string para número
-        // ${} - Utilizado para puxar variaveis dentro de strings
-        nota = Number(prompt(`Informe a nota ${conta}`))
-        soma = soma + nota
-        conta++ // conta = conta + 1
+  let nota // guarda as notas
+  let conta = 1 // conta de 1 a 10
+  let soma = 0
+  let satisfacao = 0
+  let insatisfacao = 0
 
-        if(nota >= 8){
-          satisfacao++
-        } else if(nota >= 0 && nota < 5){
-          insatisfacao++
-        } else if(nota >= 0 && nota > 10){
-          alert("Nota Invalida!!)
-        }
+  while (conta <= 10) {
+
+    // Number - converte o dado informado de string para número
+    // ${} - Utilizado para puxar variaveis dentro de strings
+    nota = Number(prompt(`Informe a nota ${conta}`))
+    soma = soma + nota
+    conta++ // conta = conta + 1
+
+    if (nota >= 8) {
+      satisfacao++
+    } else if (nota < 5) {
+      insatisfacao++
     }
 
-   
-    let media = soma / 10
+  }
 
-    //mostra o resultado
-    // toFixed(1) - Determina uma casa decimal para a variavel
-    alert(`A média das notas é ${media.toFixed(1)}`)
-    alert(`O número de satisfeitos é ${satisfacao}`)
-    alert(`O número de insatisfeitos é ${insatisfacao}`)
-      
+
+  let media = soma / 10
+
+  //mostra o resultado
+  // toFixed(1) - Determina uma casa decimal para a variavel
+  alert(`A média das notas é ${media.toFixed(1)}`)
+  alert(`O número de satisfeitos é ${satisfacao}`)
+  alert(`O número de insatisfeitos é ${insatisfacao}`)
+
 }
